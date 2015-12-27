bbbd64e83717b21a92ba3d68c1e28f47b530b3cf
Mykhailo <mykhaylo@tactile.com>
2015-12-27 04:35:20 +0200

    migrated to howler for audio api

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,17 @@
 $(function () {
     var ALPHABYTE="abcdefghijklmnopqrstuvwxyz_";
+    var sounds = {};
     ALPHABYTE.split("").forEach(function (letter) {
-        $("<audio id='" + letter + "Letter' ><source src='media/" + letter + ".ogg' type='audio/ogg' /> <source src='media/" + letter + ".mp3' type='audio/mpeg' />").appendTo("body");
-    });
+        sounds[letter]=  new Howl({
+            urls: [ "media/" + letter + ".mp3","media/" + letter + ".ogg"],
+            "onend":function(){
+              playNextLetter();
+            }
+        });
+
+     });
+
+
     function transliterate(text)
     {
         var uaToEn = {А:"A",а:"a",Б:"B",б:"b",В:"V",в:"v",Г:"H",г:"h",Ґ:"G",ґ:"g",Д:"D",д:"d",Е:"E",е:"e",Є:"Ye",є:"ye",Ж:"Zh",ж:"zh",З:"Z",з:"z",И:"Y",и:"y",І:"I",і:"i",Ї:"Yi",ї:"yi",Й:"J",й:"j",К:"K",к:"k",Л:"L",л:"l",М:"M",м:"m",Н:"N",н:"n",О:"O",о:"o",П:"P",п:"p",Р:"R",р:"r",С:"S",с:"s",Т:"T",т:"t",У:"U",у:"u",Ф:"F",ф:"f",Х:"X",х:"x",Ц:"C",ц:"c",Ч:"Ch",ч:"ch",Ш:"Sh",ш:"sh",Щ:"Shh",щ:"shh",Ю:"Yu",ю:"yu",Я:"Ya",я:"ya",Ь:"\"\"",ь:"\""};
@@ -31,21 +40,16 @@ $(function () {
                 letter='_';
             if(ALPHABYTE.indexOf(letter) !=-1)
             {
-                var node = $("#" + letter + "Letter");
-                if (node.length > 0) {
-                    node[0].play();
-                    break;
-                }
+                var sound = sounds[letter];
+                sound.play();
+                break;
+
             }
 
         }
     }
     $("form").on("submit", function (e) {
-        $("audio").each(function () {
-           var audio =  $(this)[0];
-            audio.pause();
-            audio.currentTime = 0;
-        });
+
         $("#translation").val("");
         ga('send', {
             'hitType': 'event',
@@ -60,7 +64,7 @@ $(function () {
         e.preventDefault();
 
     });
-    $("audio").on("ended", playNextLetter);
+
     setTimeout(function(){
        $("header").addClass("visible");
     },1)
